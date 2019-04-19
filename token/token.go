3e54d48fc89858335f8ac5cb4cc2bcf3014c6164
oshou <adf1985adf@gmail.com>
2019-04-19 22:57:42 +0900

    トークンと構文解析器追加

---
+++ token/token.go
@@ -0,0 +1,52 @@
+package token
+
+type TokenType string
+
+const (
+	// 異常値
+	ILLEGAL = "ILLEGAL"
+
+	// 終端
+	EOF = "EOF"
+
+	// 識別子 + リテラル
+	IDENT = "IDENT" // add, foobar, x, y...
+	INT   = "INT"
+
+	// 演算子
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
